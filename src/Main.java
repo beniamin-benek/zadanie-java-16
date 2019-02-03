bd3a0968044e452d5d0daa59665571916508105b
benek <ben.lyzn@wp.pl>
2019-02-03 21:32:27 +0100

    Dodanie drugiej metody

--- src/Main.java
+++ src/Main.java
@@ -8,20 +8,24 @@ class Main {
         ArraysChanger arraysChanger = new ArraysChanger();
 
         int[] a = {1,2,3,4,5,6}; //test
-        System.out.println(Arrays.toString(arraysChanger.giveNewArray(a)));
+        System.out.println(Arrays.toString(arraysChanger.giveNewArray(a))); //pierwsza metoda
+        arraysChanger.printNewArray(a); //druga metoda
 
         int[] b = {0}; //test
-        System.out.println(Arrays.toString(arraysChanger.giveNewArray(b)));
+        System.out.println(Arrays.toString(arraysChanger.giveNewArray(b))); //pierwsza metoda
+        arraysChanger.printNewArray(b); //druga metoda
 
         int[] c = {3, 3, -3, 4, 4, 5}; //test
-        System.out.println(Arrays.toString(arraysChanger.giveNewArray(c)));
+        System.out.println(Arrays.toString(arraysChanger.giveNewArray(c))); //pierwsza metoda
+        arraysChanger.printNewArray(c); //druga metoda
 
         Random rand = new Random();
-        int[] d = new int[rand.nextInt(10)]; //test dla losowej tablicy
+        int[] d = new int[rand.nextInt(10)]; //test dla losowej tablicy o losowym rozmiarze
         for (int i = 0; i < d.length; i++) {
             d[i] = rand.nextInt(100);
         }
-        System.out.println(Arrays.toString(arraysChanger.giveNewArray(d)));
+        System.out.println(Arrays.toString(arraysChanger.giveNewArray(d))); //pierwsza metoda
+        arraysChanger.printNewArray(d); //druga metoda
 
     }
 
